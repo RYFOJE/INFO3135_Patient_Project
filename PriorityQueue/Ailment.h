85c68304e0c9ef6c9331ca2b44ef1cb16619adfa
Ryan Jennings <ryfoje2@gmail.com>
2023-10-02 15:54:47 -0400

    Made all Ailment values const

--- PriorityQueue/Ailment.h
+++ PriorityQueue/Ailment.h
@@ -17,12 +17,12 @@ private:
 
 public:
 	Ailment(std::string name, score_t severity, score_t time_sensitivity, score_t contagiousness);
-	Ailment() : severity_(0), time_crit_(0), contagiousness_(0) , name_(""){}
+	Ailment() : severity_(0), time_crit_(0), contagiousness_(0) , name_(""){} // TODO See if there is a better way of applying this code as it is double assigning with default constructor
 
 	// Getters
-	std::string get_name() const;
-	score_t get_severity() const;
-	score_t get_time_sensitivity() const;
-	score_t get_contagiousness() const;
-	score_t get_score() const;
+	const std::string get_name() const;
+	const score_t get_severity() const;
+	const score_t get_time_sensitivity() const;
+	const score_t get_contagiousness() const;
+	const score_t get_score() const;
 };
